3544264a3c27ae7739c8cf6c9c6bb93c0aba6c69
Hamidreza Ramezani <hamidreza.it.94478.4251@gmail.com>
2023-07-09 01:51:26 +0430

    Create Basic Exercises

---
+++ Array in C#/1-/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿int[] ints= new int[3] {20,30,40};
+//ints[0] = 15;
+//ints[1] = 20;
+//ints[2]=30;
+
+Console.WriteLine(ints[2]);
+
+//
+int[,] ints1 = new int[2, 3];
+ints1[0, 0] = 5;
+ints1[0, 1] = 6;
+ints1[0, 2] = 7;
+
+ints1[1, 0] = 68;
+ints1[1,1]=9;
+ints1[1,2]=10;
+//5 6 7
+//68 9 10
+Console.WriteLine(ints1[1,2]);
+
+// 3d
+string[,,] color = new string[2, 3, 2]
+{
+    {{"red","blue"},{"green","black"},{"cryn","red blur"} },
+     {{"red1","blue1"},{"green2","black2"},{"cryn3","red blur3"} },
+};
+Console.WriteLine("color is  "+color[1,0,0]);
+
+//jagged
+int[][] students = new int[3][];
+students[0] = new int[3];
+students[1] = new int[2];
+students[2] = new int[1];
+
+students[0][0] = 22250;
+students[0][1] = 2250;
+students[0][2] = 2550;
+
+students[1][0] = 2850;
+students[1][1] = 25220;
+
+students[2][0] = 2750;
+
+Console.WriteLine("jagged :  "+ students[0][1]);
---
+++ Array in C#/1-/ConsoleApp1/Practice/Program.cs
@@ -0,0 +1,59 @@
+﻿//C# Array Exercise
+Console.WriteLine("C# Array Exercise");
+
+//Practice -1
+
+//int[] Number = new int[9];
+//for (int i = 0;i<9;i++)
+//{
+//    Console.WriteLine("Enter Your Number {0}: ",i);
+//    Number[i] = int.Parse(Console.ReadLine());
+//}
+
+//for (int b =0; b< Number.Length;b++)
+//{
+//    Console.WriteLine("Element {0} : {1}",b, Number[b]);
+//};
+
+//Practice -2
+
+//int[] numbertow= new int[3];
+//for(int i = 0; i< numbertow.Length; i++)
+//{
+//    Console.WriteLine("Enter Number :");
+//    numbertow[i]=int.Parse(Console.ReadLine());
+//}
+//for(int b =0;b<numbertow.Length;b++)
+//{
+//    Array.Sort(numbertow);
+//    Console.WriteLine("Sorted Araay {0}", numbertow[b]);
+//}
+
+//Practice -3
+int[,] Array2D = new int[3,3];
+for (int i = 0;i<3;i++)
+{
+	for (int d = 0; d < 3; d++)
+	{
+		Console.WriteLine("Add Value in Array :");
+		Array2D[i,d]=int.Parse(Console.ReadLine());
+	}
+}
+
+Console.Write("\nThe matrix is : \n");
+
+for (int i = 0; i < 3; i++)
+{
+
+    //Console.Write("\n");
+
+    for (int d = 0; d < 3; d++)
+       
+
+    {
+        Console.WriteLine("{0}\t ", Array2D[i, d]);
+       // Console.Write("\n\n");
+    }
+
+}
+   
