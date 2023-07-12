341a83553ae5a50208a01f8fcb77f1481102dc9d
Hamidreza Ramezani <hamidreza.it.94478.4251@gmail.com>
2023-07-12 23:23:12 +0430

    Add Nullabel

---
+++ Nullable in C#/Nullablel/Null Conditional oprator/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("**Null Conditional oprator**");
+
+//-1
+//Car Car = null
+
+//if (Car != null) 
+//{
+//    Console.WriteLine(Car.Name_Car);
+//}
+
+//-2
+//Car Car = new Car();
+//Car.Name_Car = "Benz";
+////check nullable [?]
+//Console.WriteLine(Car?.Name_Car);
+
+//-3
+//check or IsNullOrWhiteSpace
+
+Car Car = null;
+if(string.IsNullOrWhiteSpace(Car?.Name_Car))
+{
+    Console.WriteLine("is null");
+}
+else
+{
+    Console.WriteLine(Car.Name_Car);
+}
+
+
+
+public class Car
+{
+    public string Name_Car { get; set; }
+    public string Model_Car { get; set; }
+}
+
---
+++ Nullable in C#/Nullablel/Nullablel/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("**Naullable variabel**");
+
+
+//Error Naullable variabel
+//int a = null;
+
+int? a = null;
+a = 25;
+if (a == null)
+{
+    Console.WriteLine("Vairabel is Null");
+}
